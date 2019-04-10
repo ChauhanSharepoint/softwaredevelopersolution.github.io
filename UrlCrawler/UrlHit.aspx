b5c19818d3105b8f41a05e6c6cb9826953e61877
ChauhanSharepoint <49487787+ChauhanSharepoint@users.noreply.github.com>
2019-04-10 20:33:19 +0530

    publishing
    i am ok

---
+++ UrlCrawler/UrlHit.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UrlHit.aspx.cs" Inherits="SoftwareDeveloperSolution.UrlCrawler.UrlHit" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+                <td>
+                    Web Site Path :
+                </td>
+                <td>
+                    <asp:TextBox ID="txtWebSitePath" runat="server">http://softwaredevelopersolution.com/</asp:TextBox>
+                </td>
+                <td>
+                    Site Map Path :
+                </td>
+                <td>
+                    <asp:TextBox ID="txtSiteMapPath" runat="server">http://softwaredevelopersolution.com/sitemap1.xml</asp:TextBox>
+                </td>
+                <td>
+                    <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="5">
+                    <asp:Label ID="lblWebPage" runat="server" Text=""></asp:Label>
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ UrlCrawler/UrlHit.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Xml;
+
+namespace SoftwareDeveloperSolution.UrlCrawler
+{
+    public partial class UrlHit : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblWebPage.Text = GetHtmlPage(txtWebSitePath.Text);
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //lblWebPage.Text = GetHtmlPage(txtWebSitePath.Text);
+            ReadSiteMapFile(sender, e);
+        }
+        static string GetHtmlPage(string strURL)
+        {
+            String strResult;
+            using (WebClient client = new WebClient())
+            {
+                strResult = client.DownloadString(strURL);
+                //strResult = System.Text.Encoding.UTF8.GetString(data);
+
+                //String strResult;
+                //WebResponse objResponse;
+                //WebRequest objRequest = HttpWebRequest.Create(strURL);
+                //objResponse = objRequest.GetResponse();
+                //using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
+                //{
+                //    strResult = sr.ReadToEnd();
+                //    sr.Close();
+                //}
+            }
+            return strResult;
+        }
+
+        private void ReadSiteMapFile(object sender, EventArgs e)
+        {
+            string PageUrl = "", siteMapData = "";
+            using (WebClient wc = new WebClient())
+            {
+                wc.Encoding = System.Text.Encoding.UTF8;
+                siteMapData = wc.DownloadString(txtSiteMapPath.Text);
+            }
+            XmlDocument urldoc = new XmlDocument();
+            urldoc.LoadXml(siteMapData);
+            XmlNodeList xnList = urldoc.GetElementsByTagName("url");
+            /*Loops through the node list and prints the values of each node*/
+            foreach (XmlNode node in xnList)
+            {
+                PageUrl = node["loc"].InnerText;
+                lblWebPage.Text += PageUrl + "<br />";
+                lblWebPage.Text = GetHtmlPage(PageUrl);
+                txtWebSitePath.Text = PageUrl;
+                Thread.Sleep(30 * 1000);
+            }
+            Thread.Sleep(60 * 1000);
+            btnSubmit_Click(sender, e);
+        }
+    }
+}
